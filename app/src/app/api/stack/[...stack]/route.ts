6222c1dbe498602ddb2700cd622b5652e78d5f9b
Dunno Much <ve@theblifemovement.com>
2025-12-11 19:59:03 +0100

    feat: Add catch-all API route for Stack Auth

---
+++ app/src/app/api/stack/[...stack]/route.ts
@@ -0,0 +1,6 @@
+import { stackServerApp } from '@/lib/stack-app';
+
+export const GET = stackServerApp.apiHandler;
+export const POST = stackServerApp.apiHandler;
+export const PUT = stackServerApp.apiHandler;
+export const DELETE = stackServerApp.apiHandler;
